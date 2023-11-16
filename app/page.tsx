52b1d96abed9a14445e0e17b52401baa06a265e3
Kevin Li <jshiansongli@gmail.com>
2023-11-16 12:21:34 -0800

    responsive

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <>
 			<div className="h-screen w-screen flex flex-col justify-between bg-white">
 				<div className="flex-grow flex flex-col items-center justify-center">
-					<h1 className="text-8xl text-center font-bold text-orange-500 shimmer">
+					<h1 className="text-5xl lg:text-8xl md:text-6xl sm:text-5xl text-center font-bold text-orange-500 shimmer">
 						bubbly.design
 					</h1>
 				</div>
